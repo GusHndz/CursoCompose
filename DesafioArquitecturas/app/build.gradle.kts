fa4eac6e4e0091518c8fc546b4e8ebc09c9fc17a
Gustavo Hernandez Parra <ghernandezp@intercam.com.mx>
2023-10-05 23:54:06 -0600

    Class 2

--- DesafioArquitecturas/app/build.gradle.kts
+++ DesafioArquitecturas/app/build.gradle.kts
@@ -64,6 +64,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //coil
     implementation("io.coil-kt:coil-compose:2.4.0")
+    // viewmodel compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    //runtime-livedata
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- DesafioArquitecturas/app/src/main/java/com/mx/gustavo/hndz/desafioarquitecturas/MainActivity.kt
+++ DesafioArquitecturas/app/src/main/java/com/mx/gustavo/hndz/desafioarquitecturas/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,7 +16,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,10 +29,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ProduceStateScope
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.mx.gustavo.hndz.desafioarquitecturas.ui.theme.DesafioArquitecturasTheme
 import retrofit2.Retrofit
@@ -39,15 +51,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DesafioArquitecturasTheme {
-                val movies = produceState<List<ServerMovie>>(initialValue = emptyList()) {
-                    value = Retrofit.Builder()
-                        .baseUrl("https://api.themoviedb.org/3/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                        .create(MovieService::class.java)
-                        .getMovies()
-                        .results
-                }
+                val viewModel: MainViewModel = viewModel()
+                // LiveData
+//                val state by viewModel.state.observeAsState(MainViewModel.UiState())
+                // StateFlow
+                val state by viewModel.state.collectAsState()
+
+//                val movies = produceState<List<ServerMovie>>(initialValue = emptyList()) {
+//                    value = Retrofit.Builder()
+//                        .baseUrl("https://api.themoviedb.org/3/")
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build()
+//                        .create(MovieService::class.java)
+//                        .getMovies()
+//                        .results
+//                }
 
                 // A surface container using the 'background' color from the theme
 //                LaunchedEffect(key1 = true){
@@ -77,29 +95,30 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     ) { padding ->
-                        LazyVerticalGrid(
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            columns = GridCells.Adaptive(120.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ) {
-                            items(movies.value) {
-                                Column {
-                                    AsyncImage(
-                                        model = "https://image.tmdb.org/t/p/w500/${it.poster_path}",
-                                        contentDescription = it.title,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .aspectRatio(2 / 3f)
-                                    )
-                                    Text(
-                                        text = it.title,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(8.dp),
-                                        maxLines = 1
-                                    )
+                        if (state.loading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+
+                        }
+
+                        if (state.movie.isNotEmpty()) {
+                            LazyVerticalGrid(
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                columns = GridCells.Adaptive(120.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ) {
+//                            items(viewModel.state.movie) {
+                                // LiveData/StateFlow
+                                items(state.movie) { movie ->
+                                    MoveItems(movie = movie) {
+                                        viewModel.onMovieClick(movie)
+                                    }
                                 }
                             }
                         }
@@ -118,10 +137,68 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MoveItems(movie: ServerMovie, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.clickable {
+            onClick()
+        }) {
+        Box {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w500/${movie.poster_path}",
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+            )
+
+            if (movie.favorite) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite",
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp),
+                    tint = Color(0xFFEC0F21)
+                )
+            }
+        }
+
+        Text(
+            text = movie.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            maxLines = 1
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DesafioArquitecturasTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        MoveItems(
+            movie = ServerMovie(
+                adult = false,
+                backdrop_path = "https://image.tmdb.org/t/p/w500/1",
+                genre_ids = listOf(),
+                id = 1,
+                original_language = "en",
+                original_title = "Spider-Man: No Way Home",
+                overview = "Spider-Man: No Way Home",
+                popularity = 0.0,
+                poster_path = "https://image.tmdb.org/t/p/w500/1",
+                release_date = "2021-12-15",
+                title = "Spider-Man: No Way Home",
+                video = false,
+                vote_average = 0.0,
+                vote_count = 0,
+                favorite = true
+            )
+        ) {
+
+        }
     }
 }
